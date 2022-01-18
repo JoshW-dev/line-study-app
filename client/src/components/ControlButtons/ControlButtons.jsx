5e040e375886ca7e56528d91615516f4a6b8d414
Josh thinkpad <joshuafwade@gmail.com>
2022-01-18 13:31:32 -0700

    add laps functionality

--- client/src/components/ControlButtons/ControlButtons.jsx
+++ client/src/components/ControlButtons/ControlButtons.jsx
@@ -1,29 +1,59 @@
 import React from "react";
 import "./ControlButtons.css";
-  
+
 export default function ControlButtons(props) {
   const StartButton = (
     <div className="btn btn-one btn-start"
-         onClick={props.handleStart}>
+      onClick={props.handleStart}>
       Start
     </div>
+
   );
-  const ActiveButtons = (
+  const StartButtons = (
     <div className="btn-grp">
-      <div className="btn btn-two" 
-           onClick={props.handleReset}>
+      <div className="btn btn-one btn-start"
+        onClick={props.handleStart}>
+        Start
+      </div>
+      <div className="btn btn-two"
+        onClick={props.handleReset}>
         Reset
       </div>
-      <div className="btn btn-one" 
-           onClick={props.handlePauseResume}>
-        {props.isPaused ? "Resume" : "Pause"}
+    </div>
+  )
+
+  const UnpausedButtons = (
+    <div className="btn-grp">
+      <div className="btn btn-one"
+        onClick={props.handlePauseResume}>
+        Stop
       </div>
+      <div className="btn btn-two"
+        onClick={props.handleLap}>
+        Lap
+      </div>
+
     </div>
   );
-  
+  const PausedButtons = (
+    <div className="btn-grp">
+      <div className="btn btn-one"
+        onClick={props.handlePauseResume}>
+        Start
+      </div>
+      <div className="btn btn-two"
+        onClick={props.handleReset}>
+        Reset
+      </div>
+
+    </div>
+  );
+
+
+
   return (
     <div className="Control-Buttons">
-      <div>{props.active ? ActiveButtons : StartButton}</div>
+      <div>{props.isPaused ? PausedButtons : UnpausedButtons}</div>
     </div>
   );
 }
--- client/src/components/StopWatch/StopWatch.jsx
+++ client/src/components/StopWatch/StopWatch.jsx
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 import "./StopWatch.css";
 import Timer from "../Timer/Timer";
 import ControlButtons from "../ControlButtons/ControlButtons";
-  
+
 function StopWatch(params) {
   const [isActive, setIsActive] = useState(false);
   const [isPaused, setIsPaused] = useState(true);
   const [time, setTime] = useState(0);
-
+  const [laps, setLaps] = useState([0]);//first lap always starts at 0
   React.useEffect(() => {
     let interval = null;
-  
+
     if (isActive && isPaused === false) {
       interval = setInterval(() => {
         setTime((time) => time + 10);
@@ -22,22 +22,31 @@ function StopWatch(params) {
       clearInterval(interval);
     };
   }, [isActive, isPaused]);
-  
+
   const handleStart = () => {
     setIsActive(true);
     setIsPaused(false);
   };
-  
+
   const handlePauseResume = () => {
+    if (!isActive) {
+      setIsActive(true)
+    }
     setIsPaused(!isPaused);
     params.sendTime(time)
   };
-  
   const handleReset = () => {
     setIsActive(false);
     setTime(0);
+    setLaps([])
   };
-  
+  const handleLap = () => {
+    console.log("lap")
+    const lapsSum = laps.reduce((result, number) => result + number);
+    let newLap = time - lapsSum
+    console.log(newLap)
+    setLaps([...laps, newLap]);
+  };
   return (
     <div className="stop-watch">
       <Timer time={time} />
@@ -47,9 +56,15 @@ function StopWatch(params) {
         handleStart={handleStart}
         handlePauseResume={handlePauseResume}
         handleReset={handleReset}
+        handleLap={handleLap}
       />
+      <div className="laps">
+        {laps.map(function (time, index) {
+          return (<div >Lap {index} : {time}</div>)
+        })}
+      </div>
     </div>
   );
 }
-  
+
 export default StopWatch;
